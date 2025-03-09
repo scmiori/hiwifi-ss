ccec5d191860ea484cdb88cc6fdcb9d89565f080
scmiori <scmiori@gmail.com>
2025-03-09 20:43:15 +0800

    Update ss-test.lua

--- lib/ss-test.lua
+++ lib/ss-test.lua
@@ -3,7 +3,7 @@
 --
 require("socket")
 local https = require("ssl.https")
-local body, http_code_1, headers, status = https.request("htps://www.youtube.com")
+local body, http_code_1, headers, status = https.request("https://www.youtube.com")
 --local body, http_code_2, headers, status = https.request("https://www.facebook.com") 
 --local body, http_code_3, headers, status = https.request("https://www.google.com")
 --print(http_code_1)
